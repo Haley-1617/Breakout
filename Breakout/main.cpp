fcbd41dc7ffd8dc681bb605e2c02c816d113cf5c
Haley-1617 <alicelai2920@gmail.com>
2020-03-11 17:49:58 -0700

    Completed Breakout class and Board class.

--- Breakout/main.cpp
+++ Breakout/main.cpp
@@ -15,75 +15,307 @@
 // - Click OK.
 //
 
+#include <iostream>
+#include <cmath>
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
-int main(int argc, char const** argv)
-{
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+class Window {
+private:
+    sf::RenderWindow window;
+    sf::Vector2u windowSize;
+    std::string title;
+    bool closeWindow;
+    bool fullScreen;
+    void clear() {window.close();}
+    void setup(const std::string &title, const sf::Vector2u &size);
+    void create();
+    
+public:
+    Window() {Window("Default", sf::Vector2u(600, 800));}
+    Window(std::string title, sf::Vector2u windowSize) {setup(title, windowSize);}
+    ~Window() {clear();}
+    void clearDraw() {window.clear(sf::Color::Black);}
+    void draw(sf::Drawable &drawable) {window.draw(drawable);}
+    void display() {window.display();}
+    void Update();
+    bool getCloseWindow() {return closeWindow;}
+    bool getFullScreen() {return fullScreen;}
+    sf::Vector2u getWindowSize() {return windowSize;}
+    sf::RenderWindow* getWindow() {return &window;}
+    void toggleFullScreen();
+};
 
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile("icon.png")) {
-        return EXIT_FAILURE;
+void Window::setup(const std::string &title, const sf::Vector2u &size) {
+    this->title = title;
+    this->windowSize = size;
+    closeWindow = false;
+    fullScreen = false;
+    create();
+}
+
+void Window::create() {
+    auto style = (fullScreen ? sf::Style::Fullscreen : sf::Style::Default);
+    window.create({windowSize.x, windowSize.y, 32}, title, style);
+}
+
+void Window::Update() {
+    sf::Event e;
+    while(window.pollEvent(e)) {
+        if (e.type == sf::Event::Closed) closeWindow = true;
     }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+}
 
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile("cute_image.jpg")) {
-        return EXIT_FAILURE;
+void Window::toggleFullScreen() {
+    fullScreen = !fullScreen;
+    clear();
+    create();
+}
+
+enum class Direction{None, Left, Right};
+
+class Breakout {
+private:
+    Direction dir;
+//    sf::Vector2u position;
+    int speed;
+    int lives;
+    int score;
+    bool gameover;
+    sf::RectangleShape board;
+    
+public:
+    Breakout();
+    ~Breakout(){};
+    void render(sf::RenderWindow &window);
+    void move();
+    void tick();
+    void setDirection(Direction dir){this->dir = dir;}
+    void setScore(){score += 20;}
+    void setLives(){lives--;}
+    void lose() {gameover = true;}
+    void getGameStatus() {return gameover;}
+    sf::RectangleShape getBoard() {return board;}
+};
+
+Breakout::Breakout() {
+    board.setSize(sf::Vector2f(100.0f, 20.0f));
+//    board.setOrigin(board.getSize().x / 2, board.getSize().y / 2);
+    board.setPosition(600 - board.getSize().x / 2, 700);
+    gameover = false;
+    score = 0;
+    lives = 3;
+    speed = 10;
+    setDirection(Direction::None);
+}
+
+void Breakout::render(sf::RenderWindow &window) {
+    board.setFillColor(sf::Color::Red);
+    window.draw(board);
+}
+
+void Breakout::move() {
+    if (dir == Direction::Left && board.getPosition().x >= 0)
+        board.setPosition(board.getPosition().x - speed, board.getPosition().y);
+    else if (dir == Direction::Right &&
+             (board.getPosition().x + board.getSize().x) <= 1200)
+        board.setPosition(board.getPosition().x + speed, board.getPosition().y);
+}
+
+void Breakout::tick() {
+    if (dir == Direction::None) return;
+    move();
+}
+
+using BlockSet = std::vector<sf::RectangleShape>;
+
+class Board {
+private:
+    Breakout breakout;
+    sf::CircleShape ball;
+    std::vector<BlockSet> block;
+    int blockNum;
+    sf::Vector2i increment;
+    void initialBlock();
+    sf::Clock clock;
+    float elapsed;
+    
+public:
+    Board();
+    ~Board(){}
+    void Update();
+    void ballMovement();
+    void render(sf::RenderWindow &window);
+    void collision();
+    sf::Time getElapsed() {return clock.getElapsedTime();}
+    void restartClock() {elapsed += clock.restart().asSeconds();}
+};
+
+void Board::initialBlock() {
+    int marginY = 50, gapY = 10, gapX = 20;
+    for (int y = 0; y < block.size(); y++) {
+        int marginX = 130;
+        for (int x = 0; x < block[y].size(); x++) {
+            block[y][x].setSize(sf::Vector2f(100.0f, 20.0f));
+            block[y][x].setFillColor(sf::Color::Blue);
+            block[y][x].setPosition(marginX, marginY);
+            marginX += gapX + block[y][0].getSize().x;
+        }
+        marginY += gapY + block[y][0].getSize().y;
     }
-    sf::Sprite sprite(texture);
+}
 
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile("sansation.ttf")) {
-        return EXIT_FAILURE;
+Board::Board() : block(6, BlockSet(8)) {
+    initialBlock();
+    ball.setRadius(12);
+    ball.setOrigin(ball.getRadius(), ball.getRadius());
+    ball.setPosition(600, 700 - 50);
+    blockNum = block.size() * block[0].size();
+    increment = sf::Vector2i(5, 10);
+    clock.restart();
+    elapsed = 0.0f;
+}
+
+void Board::Update() {
+    ballMovement();
+    float timestep = 1.0f / 10;
+    if (elapsed >= timestep) {
+        breakout.tick();
+        elapsed -= timestep;
     }
-    sf::Text text("Hello SFML", font, 50);
-    text.setFillColor(sf::Color::Black);
+}
 
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile("nice_music.ogg")) {
-        return EXIT_FAILURE;
+void Board::ballMovement() {
+    int maxrangeY = block.back().front().getPosition().y;
+    int blockSizeX = block.front().front().getSize().x;
+    int blockSizeY = block.front().front().getSize().y;
+    
+//    ball enters the block range
+    if (ball.getPosition().y - ball.getRadius() <= maxrangeY + blockSizeY) collision();
+//    check if ball hit the board
+    else {
+        float testX = ball.getPosition().x, testY = ball.getPosition().y;
+        if (testX < breakout.getBoard().getPosition().x)
+            testX = breakout.getBoard().getPosition().x;
+        else if (testX > breakout.getBoard().getPosition().x +
+                 breakout.getBoard().getSize().x)
+            testX = breakout.getBoard().getPosition().x +
+            breakout.getBoard().getSize().x;
+        if (testY < breakout.getBoard().getPosition().y)
+            testY = breakout.getBoard().getPosition().y;
+        float distX = ball.getPosition().x - testX;
+        float distY = ball.getPosition().y - testY;
+        float distance = sqrt((distX * distX) + (distY * distY));
+        if (distance <= ball.getRadius()) {
+            if (testX != ball.getPosition().x) increment.x = -increment.x;
+            increment.y = -increment.y;
+        }
     }
+    float curElapsed = elapsed;
+    ball.setPosition(ball.getPosition().x + (increment.x * curElapsed),
+                     ball.getPosition().y + (increment.y * curElapsed));
+}
 
-    // Play the music
-    music.play();
 
-    // Start the game loop
-    while (window.isOpen())
-    {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Close window: exit
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
+void Board::render(sf::RenderWindow &window) {
+    for (int y = 0; y < block.size(); y++)
+        for (int x = 0; x < block[y].size(); x++)
+            window.draw(block[y][x]);
+    window.draw(ball);
+    breakout.render(window);
+}
 
-            // Escape pressed: exit
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
+void Board::collision() {
+    int blockSizeX = block.front().front().getSize().x;
+    int blockSizeY = block.front().front().getSize().y;
+    for (int y = 0; y < block.size(); y++) {
+        for (int x = 0; x < block[y].size(); x++) {
+            float testX = ball.getPosition().x, testY = ball.getPosition().y;
+//            ball is at left to the block, testX = the posX of block
+            if (testX < block[y][x].getPosition().x) testX =
+                block[y][x].getPosition().x;
+//            ball is at right to the block, testX = the posX + width of block
+            else if (testX > block[y][x].getPosition().x + blockSizeX)
+                testX = block[y][x].getPosition().x + blockSizeX;
+//            ball is at top to the block, testY = the posY of block
+            if (testY < block[y][x].getPosition().y) testY =
+                block[y][x].getPosition().y;
+//            ball is at bottom to the block, testY = the posY + height of block
+            else if (testY > block[y][x].getPosition().y + blockSizeY)
+                testY = block[y][x].getPosition().y + blockSizeY;
+            
+            float distX = ball.getPosition().x - testX;
+            float distY = ball.getPosition().y - testY;
+            float distance = sqrt((distX * distX) + (distY * distY));
+            if (distance <= ball.getRadius()) {
+//                collision happens!
+                block[y].erase(block[y].begin() + x);
+//                ball hit the corner of block
+                if (testX != ball.getPosition().x && testY != ball.getPosition().y) {
+                    increment.x = -increment.x;
+                    increment.y = -increment.y;
+                }
+//                ball hit from the top or bottom to the block
+                else if (testX == ball.getPosition().x) increment.y = -increment.y;
+//                ball hit from the left or right to the block
+                else if (testY == ball.getPosition().y) increment.x = -increment.x;
             }
         }
-
-        // Clear screen
-        window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
-        window.display();
     }
+}
 
+int main(int argc, char const** argv)
+{
+    Window m_window("Hello World", sf::Vector2u(1200, 800));
+    Board world;
+    while(!m_window.getCloseWindow()) {
+        m_window.Update();
+        m_window.clearDraw();
+        world.Update();
+        world.render(*m_window.getWindow());
+//        center vertical line
+        sf::VertexArray vert(sf::Lines, 2);
+        vert[0].position = sf::Vector2f(600, 0);
+        vert[0].color = sf::Color::Green;
+        vert[1].position = sf::Vector2f(600, 800);
+        vert[1].color = sf::Color::Green;
+        
+//        center horizontal line
+        sf::VertexArray hori(sf::Lines, 2);
+        hori[0].position = sf::Vector2f(0, 400);
+        hori[0].color = sf::Color::Green;
+        hori[1].position = sf::Vector2f(1200, 400);
+        hori[1].color = sf::Color::Green;
+        
+//        board horizontal line
+        sf::VertexArray boardHori(sf::Lines, 2);
+        boardHori[0].position = sf::Vector2f(0, 700);
+        boardHori[0].color = sf::Color::Green;
+        boardHori[1].position = sf::Vector2f(1200, 700);
+        boardHori[1].color = sf::Color::Green;
+        
+//        board
+//        sf::RectangleShape square;
+//        square.setSize(sf::Vector2f(120.0f, 20.0f));
+//        square.setFillColor(sf::Color::Red);
+////        square.setOrigin(square.getSize().x / 2, square.getSize().y / 2);
+//        square.setPosition(600.0f - square.getSize().x / 2, 700.0f);
+        
+//        ball
+//        sf::CircleShape circle(12);
+//        circle.setFillColor(sf::Color::White);
+//        circle.setOrigin(circle.getRadius(), circle.getRadius());
+//        circle.setPosition(600, 20);
+//        world.render(*m_window.getWindow());
+//        m_window.draw(circle);
+        m_window.draw(vert);
+        m_window.draw(hori);
+////        m_window.draw(boardHori);
+//        m_window.draw(square);
+        m_window.display();
+        m_window.clearDraw();
+        world.restartClock();
+    }
+    
     return EXIT_SUCCESS;
 }
